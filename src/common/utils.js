5744eb6a6e7aec5994a2d8c03215c89c14246bff
Jacques-Fry <jack_yd.com>
2021-04-05 16:41:59 +0800

    修改工具函数说明

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,52 +1,78 @@
+/**
+ * 节流
+ * @param {*} func 
+ * @param {*} delay 
+ * @returns 
+ */
 export function debounce(func, delay) {
-  let timer = null;
-  return (...args) => {
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, delay);
-  };
+    let timer = null;
+    return (...args) => {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
 }
-
+/**
+ * 日期格式化
+ * @param {*} date 
+ * @param {*} fmt 
+ * @returns 
+ */
 export function formatDate(date, fmt) {
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-  }
-  let o = {
-    'M+': date.getMonth() + 1,
-    'd+': date.getDate(),
-    'h+': date.getHours(),
-    'm+': date.getMinutes(),
-    's+': date.getSeconds()
-  };
-  for (let k in o) {
-    if (new RegExp(`(${k})`).test(fmt)) {
-      let str = o[k] + '';
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
-  }
-  return fmt;
+    let o = {
+        'M+': date.getMonth() + 1,
+        'd+': date.getDate(),
+        'h+': date.getHours(),
+        'm+': date.getMinutes(),
+        's+': date.getSeconds()
+    };
+    for (let k in o) {
+        if (new RegExp(`(${k})`).test(fmt)) {
+            let str = o[k] + '';
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+        }
+    }
+    return fmt;
 };
 
+/**
+ * 字符串补零
+ * @param {*} str 
+ * @returns 
+ */
 function padLeftZero(str) {
-  return ('00' + str).substr(str.length);
+    return ('00' + str).substr(str.length);
 };
 
-//随机颜色
+/**
+ * 获取随机颜色
+ * @returns 
+ */
 export function randomColor() {
-  return `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
+    return `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
 }
 
-// 下载文件
+/**
+ * 下载文件
+ * @param {*} data 数据
+ * @param {*} fileName 下载后的文件名
+ */
 export function download(data, fileName) {
-  var blob = new Blob([data]);
-  if (window.navigator.msSaveOrOpenBlob) {//msSaveOrOpenBlob方法返回bool值
-      navigator.msSaveBlob(blob, fileName);//本地保存
-  } else {
-      var link = document.createElement('a');//a标签下载
-      link.href = window.URL.createObjectURL(blob);
-      link.download = fileName;
-      link.click();
-      window.URL.revokeObjectURL(link.href);
-  }
+    var blob = new Blob([data]);
+    // msSaveOrOpenBlob方法返回bool值
+    if (window.navigator.msSaveOrOpenBlob) {
+        // 本地保存
+        navigator.msSaveBlob(blob, fileName);
+    } else {
+        // a标签下载
+        var link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = fileName;
+        link.click();
+        window.URL.revokeObjectURL(link.href);
+    }
 }
